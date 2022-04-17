700a75a11224562ce3f26a45c8ad0baf93a07a19
padnitri <57793978+padnitri@users.noreply.github.com>
2022-04-17 19:58:59 +0700

    [added] psudo Navbar 75%

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template class="black--text">
+  <v-app-bar class="flex-lg-row">
+    <h3 class=".text-lg-h1"> Eduvalor </h3>
+  <v-tabs
+    fixed-tabs
+    dark
+  >
+    <v-tab class="black--text"> 
+     Contents
+    </v-tab>
+    <v-tab class="black--text">
+      Create Content
+    </v-tab>
+  </v-tabs>
+  </v-app-bar>
+</template> 
