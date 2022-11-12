d8b20c0549830fb8371fed0ae44357e5c4e91887
Earty <worawit.4516@mail.kmutt.ac.th>
2022-11-13 02:23:42 +0700

    [FIX]url

--- src/pages/SignUpIn.vue
+++ src/pages/SignUpIn.vue
@@ -222,8 +222,8 @@ export default {
 
     async login() {
       const encryptpwd = require("encrypt-with-password");
-      //const password = "EDUVALOR";
-      const password = process.env.DCRYPT_SECRET;
+      const password = "EDUVALOR";
+      //const password = process.env.DCRYPT_SECRET;
       const encrypted = encryptpwd.encrypt(this.passwordLogin, password);
       console.log(encrypted);
       let user = JSON.stringify({
@@ -238,8 +238,8 @@ export default {
       };
       console.log(user);
       const res = await axios.post(
-        //process.env.VUE_APP_BACKEND_API + "/authentication/login",
-        "https://www.eduvalor.ml/backendDev/authentication/login",
+        process.env.VUE_APP_BACKEND_API + "/authentication/login",
+        //"https://www.eduvalor.ml/backendDev/authentication/login",
         user,
         config
       );
