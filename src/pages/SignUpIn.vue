4b4a2cb98e30e34d538d6ac7f2843623a536dbf2
Earty <worawit.4516@mail.kmutt.ac.th>
2022-11-13 19:47:39 +0700

    [FIX]password set

--- src/pages/SignUpIn.vue
+++ src/pages/SignUpIn.vue
@@ -222,8 +222,8 @@ export default {
 
     async login() {
       const encryptpwd = require("encrypt-with-password");
-      //const password = "EDUVALOR";
-      const password = process.env.DCRYPT_SECRET;
+      const password = "EDUVALOR";
+      //const password = process.env.DCRYPT_SECRET;
       console.log(process.env.DCRYPT_SECRET);
       console.log(password);
       const encrypted = encryptpwd.encrypt(this.passwordLogin, password);
